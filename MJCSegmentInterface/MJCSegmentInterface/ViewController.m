97cfb737fc924772e245ea4ceae6cff111d3b95b
MJC <292251588@qq.com>
2016-10-28 10:13:49 +0800

    shishi

--- MJCSegmentInterface/MJCSegmentInterface/ViewController.m
+++ MJCSegmentInterface/MJCSegmentInterface/ViewController.m
@@ -16,14 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //啦啦啦
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
