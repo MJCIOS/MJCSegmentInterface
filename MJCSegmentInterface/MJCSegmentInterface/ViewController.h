2933b4b973c3945b9c9b39d65894896b4379d620
MJC <292251588@qq.com>
2016-11-20 23:20:24 +0800

    QQ修改

--- MJCSegmentInterface/MJCSegmentInterface/ViewController.h
+++ MJCSegmentInterface/MJCSegmentInterface/ViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by mjc on 16/10/28.
 //  Copyright © 2016年 MJC. All rights reserved.
-//  有啥问题加我QQ: 1611891567 一起交流,我是菜菜..求大神指教
+//  有啥问题加我QQ: 292251588 一起交流,我是菜菜..求大神指教
 
 #import <UIKit/UIKit.h>
 
